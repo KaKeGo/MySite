c4c4237ec79718cd5463066191822ba26a1c8051
KaKeGo <funfcia@hotmail.com>
2022-03-18 15:17:15 +0100

    add blog app to settings and base dir for templates

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Myapps
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
