a0f639ac3459ec0b8835053bd33c34f999bd7d68
KaKeGo <funfcia@hotmail.com>
2022-03-18 20:14:56 +0100

    add footer, navbar, style detail blog page and add css, image to static

--- mysite/blogs/urls.py
+++ mysite/blogs/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from .views import (
     BlogView,
+    BlogDetailView,
 )
 
 
@@ -9,4 +10,5 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('', BlogView.as_view(), name='blog'),
+    path('detail/<slug:slug>/', BlogDetailView.as_view(), name='detail'),
 ]
