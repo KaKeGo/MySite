bdbdc678591d8b8ccdea774d97ef323792f3998c
KaKeGo <funfcia@hotmail.com>
2022-03-18 15:52:48 +0100

    add bootstrap to html, create blog view, create static and media dir

--- mysite/blogs/urls.py
+++ mysite/blogs/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 
-# from .views import (
-#
-# )
+from .views import (
+    BlogView,
+)
 
 
 app_name = 'blogs'
 
 urlpatterns = [
-
+    path('', BlogView.as_view(), name='blog'),
 ]
--- mysite/blogs/views.py
+++ mysite/blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+
+class BlogView(generic.ListView):
+    template_name = 'blogs/blog.html'
+
