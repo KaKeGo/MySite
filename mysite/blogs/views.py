9035c14731faa494faa1f35575f66009aa4ac969
KaKeGo <funfcia@hotmail.com>
2022-03-18 18:30:54 +0100

    add model and add to admin and add model to view and add blog page to navbar

--- mysite/blogs/views.py
+++ mysite/blogs/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 
+from .models import Blog
+
 # Create your views here.
 
 
 class BlogView(generic.ListView):
     template_name = 'blogs/blog.html'
+    model = Blog
 
