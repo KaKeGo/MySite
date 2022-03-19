fad6efe26fba6691ee1b392ee8dadd8c2f9ecded
KaKeGo <funfcia@hotmail.com>
2022-03-19 11:44:32 +0100

    add profile view

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 
-# from .views import (
-#
-# )
+from .views import (
+    ProfileView,
+)
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from .models import Model
 
 # Create your views here.
+
+
+class ProfileView(generic.ListView):
+    template_name = 'accounts/profile.html'
+    model = Model
