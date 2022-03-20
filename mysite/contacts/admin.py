f5ddf591028371e1c33596d8234019b65f1283dc
KaKeGo <funfcia@hotmail.com>
2022-03-20 21:28:41 +0100

    add model to admin

--- mysite/contacts/admin.py
+++ mysite/contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import ContactMessage, Contact
 # Register your models here.
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'postman', 'create_on', 'is_active')
