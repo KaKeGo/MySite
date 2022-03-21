0d1635d755a8e583e0a16830d73879b3a5e5eb0e
KaKeGo <funfcia@hotmail.com>
2022-03-21 11:42:17 +0100

    login required add

--- mysite/contacts/urls.py
+++ mysite/contacts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 
 from .views import (
     ContactsView,
@@ -14,7 +15,7 @@ app_name = 'contacts'
 urlpatterns = [
     path('', ContactsView.as_view(), name='contact'),
     path('message/', ContactMessageCreateView.as_view(), name='message'),
-    path('message/list/', ContactMessgesListView.as_view(), name='list'),
-    path('message/<slug:slug>/', ContactMessageDetailView.as_view(), name='detail'),
-    path('message/<slug:slug>/delete/', ContactMessageDeleteView.as_view(), name='delete'),
+    path('message/list/', login_required(ContactMessgesListView.as_view()), name='list'),
+    path('message/<slug:slug>/', login_required(ContactMessageDetailView.as_view()), name='detail'),
+    path('message/<slug:slug>/delete/', login_required(ContactMessageDeleteView.as_view()), name='delete'),
 ]
