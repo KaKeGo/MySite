30330e3cb24e2b0c709e645b83cc353d132f51c2
KaKeGo <funfcia@hotmail.com>
2022-03-21 17:34:08 +0100

    small bootstrap upgrade

--- mysite/contacts/urls.py
+++ mysite/contacts/urls.py
@@ -14,7 +14,7 @@ app_name = 'contacts'
 
 urlpatterns = [
     path('', ContactsView.as_view(), name='contact'),
-    path('message/', ContactMessageCreateView.as_view(), name='message'),
+    path('message/', login_required(ContactMessageCreateView.as_view()), name='message'),
     path('message/list/', login_required(ContactMessgesListView.as_view()), name='list'),
     path('message/<slug:slug>/', login_required(ContactMessageDetailView.as_view()), name='detail'),
     path('message/<slug:slug>/delete/', login_required(ContactMessageDeleteView.as_view()), name='delete'),
