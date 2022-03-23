03b17d0169a2ba5db69ceb0daec3ac7e5a631754
KaKeGo <funfcia@hotmail.com>
2022-03-23 16:25:59 +0100

    category model

--- mysite/blogs/models.py
+++ mysite/blogs/models.py
@@ -6,10 +6,29 @@ from accounts.models import CustomUser
 # Create your models here.
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=60, null=True, blank=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        verbose_name = 'Category'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blogs:detail', kwargs={'slug':self.slug})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
 class Blog(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     body = models.TextField()
     image = models.ImageField(upload_to='blog_images', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True, blank=True, null=True)
     create_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
