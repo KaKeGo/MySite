551bdcad0df20d9cbf87edb886976c9223b29d5e
KaKeGo <funfcia@hotmail.com>
2022-03-27 11:23:57 +0200

    skills api work

--- mysite/about/api/urls.py
+++ mysite/about/api/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 from .views import (
     AboutLisPosApiView,
     AboutDelUpdApiView,
+    SkillLisPosApiView,
+    SkillUpdDelApiView,
 )
 
 app_name = 'about-api'
 
 urlpatterns = [
     path('', AboutLisPosApiView.as_view(), name='api-about-list-post'),
-    # path('blogs/', BlogApiView.as_view(), name='api-blog-list-post'),
+    path('skills/', SkillLisPosApiView.as_view(), name='api-skills-list-post'),
     path('del-upd/<int:id>/', AboutDelUpdApiView.as_view(), name='api-about-update-delete'),
-    # path('blogs/<int:id>/', BlogApiDelUpdView.as_view(), name='api-blog-update-delete'),
+    path('skills/<int:id>/', SkillUpdDelApiView.as_view(), name='api-blog-skills-delete'),
 ]
--- mysite/about/api/views.py
+++ mysite/about/api/views.py
@@ -6,6 +6,34 @@ from about.models import About, Skills
 from .serializers import AboutSerializer, SkillsSerializer
 
 
+class SkillUpdDelApiView(generics.GenericAPIView, mixins.ListModelMixin, mixins.UpdateModelMixin,
+                         mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = SkillsSerializer
+    queryset = Skills.objects.all()
+    lookup_field = 'id'
+
+    def get(self, request, id):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def put(self, request, id):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
+class SkillLisPosApiView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    serializer_class = SkillsSerializer
+    queryset = Skills.objects.all()
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
 class AboutDelUpdApiView(generics.GenericAPIView, mixins.ListModelMixin, mixins.UpdateModelMixin,
                          mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
     serializer_class = AboutSerializer
