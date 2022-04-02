1e4a4ba01a33583ca568504e415ddd075fccdc5f
KaKeGo <funfcia@hotmail.com>
2022-04-02 23:07:53 +0200

    info8

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kakedb',
+        'USER': 'kakego',
+        'PASSWORD': 'daniel79135',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -141,7 +145,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
